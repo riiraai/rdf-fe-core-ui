a2418184b9f510e88f41653df76a6e432088fbda
riiraai <ari35210@gmail.com>
2025-03-06 00:39:34 +0700

    feat(components): consolidate component exports into a single index file

---
+++ src/components/index.ts
@@ -0,0 +1,16 @@
+/**
+ * Components core ui
+ */
+export * from "./ui/alert";
+export * from "./ui/badge";
+export * from "./ui/breadcrumb";
+export * from "./ui/card";
+export * from "./ui/button";
+export * from "./ui/component-wrapper";
+export * from "./ui/toast";
+export * from "./ui/typography";
+
+/**
+ * Components navigation
+ */
+export * from "./ui/navigation";
--- src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./components/ui/alert";
-export * from "./components/ui/badge";
-export * from "./components/ui/breadcrumb";
-export * from "./components/ui/card";
-export * from "./components/ui/button";
